f90f5d8a4a92bfc6ce67701a025146be46053b2e
Gabriel Gomes <gabriel@mpsouza.com.br>
2025-07-03 17:13:00 -0300

    refactor: reorganizar estrutura do aplicativo e atualizar dependências

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
-import 'react-native-gesture-handler';
+// App.tsx
+import 'react-native-gesture-handler';        
+import { enableScreens } from 'react-native-screens';  
+enableScreens();                             
+
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
-
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
-      <AppNavigator />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
